21b9ec04ea940fc506a3ef39e2666545f73c9e37
tiber81-star <tiber81@hotmail.fr>
2026-09-10 20:25:26 +0200

    Update stripe-webhook.js

--- functions/stripe-webhook.js
+++ functions/stripe-webhook.js
@@ -1,3 +1,7 @@
+export async function onRequestGet(context) {
+  return new Response('Fonction OK (GET)', { status: 200 });
+}
+
 export async function onRequestPost(context) {
-  return new Response('Fonction OK', { status: 200 });
+  return new Response('Fonction OK (POST)', { status: 200 });
 }
